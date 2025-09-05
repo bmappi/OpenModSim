af6dfdfd87f4051c78034d1f95ff5df0072d6a1a
bmappi <166033030+bmappi@users.noreply.github.com>
2025-09-05 12:04:26 +0000

    remove aqt

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,5 +1,5 @@
 cd /workspaces/OpenModSim/
 cmake -S omodsim -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release
-cmake --build build --config 
+cmake --build build --config Release
 
 echo "Now run build/omodsim from vnc"
